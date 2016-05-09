4673c5830c547bbe304dfa74f91d61969f0ace24
aethel <mpankiewicz@infusion.com>
2016-05-09 15:36:45 +0200

    sass setup and compilation, button styling, font face

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 gulp.task('styles',function(){
 	gulp.src('styles/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(gulp.dest('styles/css/'));
 });
 
