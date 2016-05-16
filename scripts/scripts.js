9cdea348b621eae2a276de0db2ac0831e23bd42d
aethel <mpankiewicz@infusion.com>
2016-05-16 13:40:55 +0200

    button class toggler

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,14 +3,18 @@
     window.addEventListener('load', function() {
         InitSlider();
     }, false);
+
+
 }());
 
 var InitSlider = function() {
     var nextBtn = document.querySelector('.js-next'),
-    		prevBtn = document.querySelector('.js-prev'),
+        prevBtn = document.querySelector('.js-prev'),
         viewportSlider = document.querySelector('.js-viewport-slider'),
-				viewPortWidth = 880;
+        viewportSliderBtns = viewportSlider.querySelectorAll('.js-slider-btn'),
+        viewPortWidth = 880;
 
+    console.log(viewportSliderBtns);
     nextBtn.addEventListener('click', function() {
         if (-viewportSlider.offsetLeft > viewportSlider.offsetWidth - viewPortWidth) {
             return;
@@ -26,4 +30,36 @@ var InitSlider = function() {
         }
     }, false);
 
+		viewportSlider.addEventListener('click', function(e){
+			classToggler(e, 'js-slider-btn', 'is-active');
+		}, false);
 };
+
+var classToggler = function(event, targetClass, newClass) {
+    var correctTarget = event.target != event.currentTarget && event.target.classList.contains(targetClass) ? true : false;
+    if (correctTarget) {
+    event.target.classList.toggle(newClass);
+    }
+}
+
+
+
+
+
+
+// //slider motion blur
+// var filters = document.querySelector('.filters'),
+// 		defs = filters.querySelector('defs'),
+// 		blur = defs.querySelector('#blur'),
+// 		blurFilter = blur.firstElementChild;
+//
+// var blurTargets = document.querySelectorAll('.js-blur');
+// for (var i=0; i<blurTargets; i++){
+// 	var blurClone = blur.cloneNode(true),
+// 			blurId = 'blur' + i;
+// 			blurClone.setAttribute('id',blurId);
+// 			defs.appendChild(blurClone);
+//
+// 			var filter = "url(#"+blurId")";
+// 			blurTargets[i].setAttribute('style','filter: -webkit-filter:'+filter+',filter:'+filter);
+// }
